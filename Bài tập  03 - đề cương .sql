6751f188fe0213699623947891164860e195aa99
ngocnguyen1992 <101922329+ngocnguyen1992@users.noreply.github.com>
2022-11-09 23:29:36 +0700

    Update Bài tập  03 - đề cương .sql

--- Bài tập  03 - đề cương .sql
+++ Bài tập  03 - đề cương .sql
@@ -1,15 +1,20 @@
 /*Liệt kê ra các hoá đơn trong tháng 9/2010 (Ngay_Ct, So_Ct, Dien_Giai0, Doanh thu, tiền thuế, Tổng tiền).*/
 
-SELECT * FROM dbo.BanHang 
+SELECT Ngay_Ct, So_Ct, Dien_Giai, Doanh_thu, Thue, Tien 
+FROM dbo.BanHang 
 WHERE MONTH(Ngay_Ct) = 9 AND YEAR(Ngay_Ct) = 2013
 ORDER BY Ngay_Ct
+
+-- Em đã lấy 1 số cột theo yêu cầu và format lại đúng quy tắc
+
 ----
-/*Liệt kê ra các hoá đơn do NV kinh doanh Nguyễn Văn A bán vào tháng 9/2010. */
+/*Liệt kê ra các hoá đơn do NV kinh doanh Nguyễn Văn A bán vào tháng 9/2013. */
 
 SELECT nv.Ten_NV, bh.Ngay_Ct, bh.Ma_Nv, bh.So_Ct
 FROM dbo.BanHang bh JOIN dbo.DmNv nv ON bh.Ma_Nv = nv.Ma_Nv
-WHERE nv.Ten_NV = 'Tên Nhân Viên 882' AND MONTH(bh.Ngay_Ct)=9
-GROUP BY nv.Ten_Nv, bh.Ngay_Ct, bh.Ma_Nv, bh.So_Ct
+WHERE nv.Ten_NV = N'Nguyễn Văn A' AND MONTH(bh.Ngay_Ct)= 9 AND YEAR(Ngay_Ct) = 2013
+
+-- Em đã filter Nguyễn Văn A và thêm N'' cho chuỗi tiếng việt có dấu (NVARCHAR) và bỏ GROUP BY nhiều cấp ko cần thiết
 
 ---
 /*Liệt kê ra 3 mặt hàng bán chạy nhất trong tháng 9/2013.*/
@@ -52,18 +57,22 @@ INTO #temp
 FROM dbo.DmNv
 
 SELECT * FROM #temp 
-WHERE Tuoi >= 55 AND Gioi_Tinh = 'Nam' OR Tuoi >= 50 AND Gioi_Tinh = 'Nu' 
+WHERE (Tuoi >= 55 AND Gioi_Tinh = 'Nam') OR (Tuoi >= 50 AND Gioi_Tinh = 'Nu')
 ORDER BY Ma_Nv
-IF OBJECT_ID('tempdb..#temp') IS NOT NULL DROP TABLE #temp
+
+-- Em thêm ngoặc đoạn WHERE (AND) OR (AND)
+
 
 --Tạo một TABLE tạm #tblCt bằng câu lệnh CREATE gồm các field : Ma_Ct, Ngay_ct, So_Ct, Ma_Vt, So_Luong, Don_Gia, Thanh_tien, Ma_Kho, Ma_Dt, Ma_Nx, Ma_Tte
 --INSERT dữ liệu của các chứng từ hoá đơn, chứng từ nhập mua và chi phí vào bản tạm #tblCt.
+
+
 
 SET NOCOUNT ON;
 DROP TABLE IF EXISTS #tblCt
 
 CREATE TABLE #tblCt 
-(Id INT, Ma_Ct VARCHAR(16), Ngay_Ct DATE, So_Ct VARCHAR(16), So_Luong INT, Don_Gia INT,
+(Id INT, Ma_Ct VARCHAR(16), Ma_Vt VARCHAR(16), Ngay_Ct DATE, So_Ct VARCHAR(16), So_Luong INT, Don_Gia INT,
 Thanh_Tien INT, Ma_Kho VARCHAR(16), Ma_Dt VARCHAR(16), Ma_Nx VARCHAR(16), Ma_Tte VARCHAR(16));
 
 DECLARE  @_Don_Gia NUMERIC(18), @_Ma_Ct VARCHAR(16)
@@ -73,41 +82,45 @@ DECLARE  @_Don_Gia NUMERIC(18), @_Ma_Ct VARCHAR(16)
 WHILE @_i < 100
 BEGIN
 	SELECT @_Ma_Ct = 'Ct' + FORMAT(@_i, '0####');
-	SELECT @_Ma_Kho = 'KHO' + FORMAT(@_i, '0####');
 	SELECT @_Ma_Nx = 'Nx' + FORMAT(@_i, '0####');
+	SELECT @_Ma_Kho = 'Kho' + FORMAT(@_i, '0####');
 	SELECT @_Ma_Tte = 'Tte' + FORMAT(@_i, '0####');
 	SET @_Don_Gia = ROUND(RAND()*10000, 0);
 
-    INSERT #tblCt (Id, Ma_Ct, Don_Gia, Ma_Kho, Ma_Nx, Ma_Tte) 
-    VALUES(@_i, @_Ma_Ct, @_Don_Gia, @_Ma_Kho, @_Ma_Nx, @_Ma_Tte );
+    INSERT #tblCt (Id, Ma_Ct, Ma_Kho, Don_Gia, Ma_Nx, Ma_Tte) 
+    VALUES(@_i, @_Ma_Ct, @_Ma_Kho, @_Don_Gia, @_Ma_Nx, @_Ma_Tte );
     SET @_i +=1;
 END
 
 --Dùng câu truy vấn SQL thêm vào bảng #tblCt các filed Ten_Vt, Ten_Kho, Ten_Dt.
 --Update dữ liệu cho các field Ten_Vt, Ten_Kho, Ten_Dt của bảng #tblCt.
-
-ALTER TABLE #tblCt ADD Ten_Vt NVARCHAR(64), Ten_Kho NVARCHAR(64), Ten_Dt NVARCHAR(64)
-
-;WITH tbl AS(
-    SELECT ROW_NUMBER() OVER(ORDER BY Ngay_Ct) AS _RowNo
-        , Ten_Vt, Ten_Kho, Ten_Dt
-    FROM #tblCt
-)
-    UPDATE tbl
-    SET Ten_Vt = N'Tên Vật Tư ' + FORMAT(_RowNo, '0####')
-	, Ten_Kho = N'Tên Kho ' + FORMAT(_RowNo, '1##7#')
-	, Ten_Dt = N'Tên Khách Hàng ' + FORMAT(_RowNo, '2##2#');
-
 --Lấy dữ liệu từ bảng bán hàng qua bảng #tblCt
+ALTER TABLE #tblCt ADD Ten_Vt NVARCHAR(128), Ten_Kho NVARCHAR(128), Ten_Dt NVARCHAR(128)
+
 UPDATE #tblCt 
 SET #tblCt.Ngay_Ct = bh.Ngay_Ct
     , #tblCt.So_Ct = bh.So_Ct
     , #tblCt.So_Luong = bh.So_Luong
     , #tblCt.Ma_Dt = bh.Ma_Dt
     , #tblCt.Thanh_Tien = bh.Tien
-FROM (SELECT Id, Ngay_Ct, So_Ct, So_Luong, Ma_Dt, Tien FROM dbo.BanHang) bh
+    , #tblCt.Ma_Vt = bh.Ma_Vt
+FROM (SELECT Id, Ngay_Ct, So_Ct, So_Luong, Ma_Dt, Ma_Vt, Tien FROM dbo.BanHang) bh
 WHERE bh.Id = #tblCt.Id
 
+UPDATE #tblCt
+SET #tblCt.Ten_Kho = wh.Ten_Kho
+FROM (SELECT Ma_Kho, Ten_Kho FROM dbo.WareHouse) wh
+WHERE #tblCt.Ma_Kho = wh.Ma_Kho
+
+UPDATE #tblCt
+SET #tblCt.Ten_Vt = vt.Ten_Vt
+FROM (SELECT Ten_Vt, Ma_Vt FROM dbo.DmVt) vt
+WHERE #tblCt.Ma_Vt = vt.Ma_Vt
+
+UPDATE #tblCt
+SET #tblCt.Ten_Dt = dt.Ten_Dt
+FROM (SELECT Ten_Dt, Ma_Dt FROM dbo.DmDt) dt
+WHERE #tblCt.Ma_Dt = dt.Ma_Dt
 
 --Copy dữ liệu của #tblCt qua #CtHdCopy rồi xoá hết dữ liệu của #tblCt.
 
@@ -118,13 +131,5 @@ INTO #CtHdCopy
 FROM #tblCt
 TRUNCATE TABLE #tblCt
 
--- Do em thấy Insert Tiền random ko đúng logic nên có thử lấy #CthdCopy.Don_Gia * bh.So_Luong = #CtHdCopy.Thanh_Tien bắt theo So_Ct
-
---UPDATE #CtHdCopy
---SET #CtHdCopy.Thanh_Tien = #CtHdCopy.Don_Gia * bh.So_Luong FROM (SELECT So_Luong, So_Ct FROM dbo.BanHang) bh 
---WHERE #CtHdCopy.So_Ct = bh.So_Ct
-
 SELECT * FROM #CtHdCopy 
 IF OBJECT_ID('tempdb..#CtHdCopy') IS NOT NULL DROP TABLE #CtHdCopy
-
-
